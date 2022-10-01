1a3404d38640caff4a669d547547661c1d2b79c3
Samu <sqyber@gmail.com>
2022-10-01 21:33:03 +0300

    namespace

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -3,56 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+namespace RemixGame.Code
 {
-    public Rigidbody2D rb;
 
-    public Transform groundCheck;
-
-    public LayerMask groundlayer;
-
-    private float horizontal;
-
-    private float speed = 8f;
-
-    private float jumpingPower = 8f;
-
-   
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        
-    }
+        public Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
-    {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        
-    }
+        public Transform groundCheck;
 
-    //Groundcheck using empty object under characters feet. Checks overlaps within a circle
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundlayer);
-    }
+        public LayerMask groundlayer;
 
-    // jump action with rigidbody velocity applying force directly
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && IsGrounded())
+        private float horizontal;
+
+        private float speed = 8f;
+
+        private float jumpingPower = 8f;
+
+
+        // Start is called before the first frame update
+        void Start()
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+
         }
 
-        if (context.canceled && rb.velocity.y > 0f)
+        // Update is called once per frame
+        void Update()
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
         }
+
+        //Groundcheck using empty object under characters feet. Checks overlaps within a circle
+        private bool IsGrounded()
+        {
+            return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundlayer);
+        }
+
+        // jump action with rigidbody velocity applying force directly
+        public void Jump(InputAction.CallbackContext context)
+        {
+            if (context.performed && IsGrounded())
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            }
+
+            if (context.canceled && rb.velocity.y > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            }
+        }
+
+        public void Move(InputAction.CallbackContext context)
+        {
+            horizontal = context.ReadValue<Vector2>().x;
+        }
+
     }
-    public void Move(InputAction.CallbackContext context)
-    {
-        horizontal = context.ReadValue<Vector2>().x;
-    }
-    
 }
