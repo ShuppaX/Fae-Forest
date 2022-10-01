4ee6a0854a2566585433598165484d402d3a2a91
Samu <sqyber@gmail.com>
2022-10-01 21:14:17 +0300

    Destroyed old project completely and made a new one
    Movement and jump function now

---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public Transform groundCheck;
+
+    public LayerMask groundlayer;
+
+    private float horizontal;
+
+    private float speed = 8f;
+
+    private float jumpingPower = 8f;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        
+    }
+
+    //Groundcheck using empty object under characters feet. Checks overlaps within a circle
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundlayer);
+    }
+
+    // jump action with rigidbody velocity applying force directly
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+    
+}
