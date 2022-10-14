a907e8770f6515ae16b84f7eab0780a201128b43
Samu <sqyber@gmail.com>
2022-10-14 21:11:58 +0300

    Added random patrol movement to enemy ai

--- Assets/Code/EnemyAi.cs
+++ Assets/Code/EnemyAi.cs
@@ -8,10 +8,9 @@ namespace RemixGame
 {
     public class EnemyAi : MonoBehaviour
     {
-        
+
         //headers look nice in editor
-        [Header("Pathfinding")]
-        public GameObject chara1;
+        [Header("Pathfinding")] public GameObject chara1;
         public Transform character1coord;
         public GameObject chara2;
         public Transform character2coord;
@@ -19,115 +18,130 @@ namespace RemixGame
         public float activateDistance = 50f;
         public float pathUpdateSeconds = 0.5f;
 
-        [Header("Physics")]
-        public float speed = 200f;
+        [Header("Patrol")] 
+        public float patrolAcceleration = 2f;
+        public float patrolmaxSpeed = 5f;
+        private Vector2 patrolmovement;
+        private float patroltimeLeft;
+        
+        [Header("Physics")] public float speed = 200f;
         public float nextWaypointDistance = 3f;
         public float jumpNodeHeightRequirement = 0.8f;
         public float jumpModifier = 0.3f;
         public float jumpCheckOffset = 0.1f;
 
-        [Header("Custom Behavior")]
-        public bool followEnabled = true;
+        [Header("Custom Behavior")] public bool followEnabled = true;
         public bool jumpEnabled = true;
         public bool directionLookEnabled = true;
 
+        //Other variables
         private Path path;
         private int currentWaypoint = 0;
         RaycastHit2D isGrounded;
         Seeker seeker;
         Rigidbody2D rb;
 
-    public void Start()
-    {
-        seeker = GetComponent<Seeker>();
-        rb = GetComponent<Rigidbody2D>();
+        public void Start()
+        {
+            seeker = GetComponent<Seeker>();
+            rb = GetComponent<Rigidbody2D>();
+            
 
-        InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
-    }
-
-    private void FixedUpdate()
-    {
-        if (chara1.activeSelf)
-        {
-            target = character1coord;
-        }
-        else
-        {
-            target = character2coord;
-        }
-        
-        if (TargetInDistance() && followEnabled)
-        {
-            PathFollow();
-        }
-    }
-
-    private void UpdatePath()
-    {
-        if (followEnabled && TargetInDistance() && seeker.IsDone())
-        {
-            seeker.StartPath(rb.position, target.position, OnPathComplete);
-        }
-    }
-
-    private void PathFollow()
-    {
-        if (path == null)
-        {
-            return;
+            InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
         }
 
-        // Reached end of path
-        if (currentWaypoint >= path.vectorPath.Count)
+        private void FixedUpdate()
         {
-            return;
-        }
-
-        // See if colliding with anything
-        Vector3 startOffset = transform.position - new Vector3(0f, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
-        isGrounded = Physics2D.Raycast(startOffset, -Vector3.up, 0.05f);
-        
-        // Direction Calculation
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        Vector2 force = direction * (speed * Time.deltaTime);
-
-        // Jump
-        if (jumpEnabled && isGrounded)
-        {
-            if (direction.y > jumpNodeHeightRequirement)
+            if (chara1.activeSelf)
             {
-                rb.AddForce(Vector2.up * (speed * jumpModifier));
+                target = character1coord;
+            }
+            else
+            {
+                target = character2coord;
+            }
+
+            if (TargetInDistance() && followEnabled)
+            {
+                PathFollow();
+            }
+            else
+            {
+                //TODO PATROL
+                Patrolmove();
+                rb.AddForce(patrolmovement * patrolmaxSpeed);
             }
         }
 
-        // Movement
-        rb.AddForce(force);
-
-        // Next Waypoint
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
-        if (distance < nextWaypointDistance)
+        private void UpdatePath()
         {
-            currentWaypoint++;
-        }
-
-        // Direction Graphics Handling prob very bad
-        if (directionLookEnabled)
-        {
-            if (rb.velocity.x > 0.05f)
+            if (followEnabled && TargetInDistance() && seeker.IsDone())
             {
-                transform.localScale = new Vector3(-1f * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-            else if (rb.velocity.x < -0.05f)
-            {
-                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+                seeker.StartPath(rb.position, target.position, OnPathComplete);
             }
         }
-    }
 
-    private bool TargetInDistance()
-    {
-        return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
-    }
+        private void PathFollow()
+        {
+            if (path == null)
+            {
+                return;
+            }
+
+            // Reached end of path
+            if (currentWaypoint >= path.vectorPath.Count)
+            {
+                return;
+            }
+
+            // See if colliding with anything
+            Vector3 startOffset = transform.position -
+                                  new Vector3(0f, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffset);
+            isGrounded = Physics2D.Raycast(startOffset, -Vector3.up, 0.05f);
+
+            // Direction Calculation
+            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+            Vector2 force = direction * (speed * Time.deltaTime);
+
+            // Jump
+            if (jumpEnabled && isGrounded)
+            {
+                if (direction.y > jumpNodeHeightRequirement)
+                {
+                    rb.AddForce(Vector2.up * (speed * jumpModifier));
+                }
+            }
+
+            // Movement
+            rb.AddForce(force);
+
+            // Next Waypoint
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+            if (distance < nextWaypointDistance)
+            {
+                currentWaypoint++;
+            }
+
+            // Direction Graphics Handling prob very bad
+            if (directionLookEnabled)
+            {
+                if (rb.velocity.x > 0.05f)
+                {
+                    transform.localScale = new Vector3(-1f * Mathf.Abs(transform.localScale.x), transform.localScale.y,
+                        transform.localScale.z);
+                }
+                else if (rb.velocity.x < -0.05f)
+                {
+                    transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y,
+                        transform.localScale.z);
+                }
+            }
+        }
+
+        private bool TargetInDistance()
+        {
+            return Vector2.Distance(transform.position, target.transform.position) < activateDistance;
+        }
 
         private void OnPathComplete(Path p)
         {
@@ -135,8 +149,22 @@ namespace RemixGame
             {
                 path = p;
                 currentWaypoint = 0;
-            } 
-        } 
+            }
+        }
+
+        /// <summary>
+        /// Patrol methods
+        /// </summary>
+
+        void Patrolmove()
+        {
+            patroltimeLeft -= Time.deltaTime;
+            if (patroltimeLeft <= 0)
+            {
+                patrolmovement = new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f));
+                patroltimeLeft += patrolAcceleration;
+            }
+        }
     }
 }
     
