7739c1629010a9a28b0a2fcdaead42b167bc5927
Samu <sqyber@gmail.com>
2022-11-14 14:50:13 +0200

    fixed blocks on first 7 levels + minor enemy tuning

--- Assets/Code/Enemy scripts/MinibossProjectile.cs
+++ Assets/Code/Enemy scripts/MinibossProjectile.cs
@@ -14,48 +14,19 @@ namespace RemixGame
         private Rigidbody2D projectileRb;
 
         private bool mbFacingLeft;
-        
-       
+
+
 
         private void Start()
         {
 
             projectileRb = gameObject.GetComponent<Rigidbody2D>();
-            Vector2 direction = new Vector2(( FindObjectOfType<Character>().gameObject.transform.position.x - projectileRb.transform.position.x), 0);
+            Vector2 direction =
+                new Vector2(
+                    (FindObjectOfType<Character>().gameObject.transform.position.x - projectileRb.transform.position.x),
+                    0);
             projectileRb.velocity = direction.normalized * Speed;
-            Debug.Log("testi" + player1.transform.position);
-            /*
-            if (miniboss.GetComponent<Miniboss>().IsFacingLeft)
-            {
-                mbFacingLeft = true;
-            } 
-            
-            else if (!miniboss.GetComponent<Miniboss>().IsFacingLeft)
-            {
-                mbFacingLeft = false;
-            }
-            */
         }
-
-        // This is used for the movement of the projectile and the direction is taken from
-        // the initial position of the projectile spawn
-        
-        /*
-        private void FixedUpdate()
-        {
-            if (!mbFacingLeft)
-            {
-                projectileRb.velocity = transform.right * Speed;
-                
-            }
-
-            if (mbFacingLeft)
-            {
-                projectileRb.velocity = transform.right * (-1 * Speed);
-
-            }
-        }
-        */
 
         // This is used to destroy the projectile if it collides with something
         // that it can collide with.
